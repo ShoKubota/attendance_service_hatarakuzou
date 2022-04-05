9a0c6f740f28e6dc91a5addcd26489db45456361
ShoKubota <sh2o.1114@mineo.jp>
2022-04-05 17:28:07 +0900

    add: usersController #5

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,22 +1,45 @@
 class Admin::UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
+
   def index
+    @users = User.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path
+    else
+      render :new
+    end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
+    if @user.update(user_params)
+      redirect_to admin_users_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @user.destroy!
   end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confarmation, :salary)
 end
