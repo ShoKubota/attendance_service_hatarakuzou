8849da15b6a258a9cab922982005475661a84508
ShoKubota <sh2o.1114@mineo.jp>
2022-04-09 09:51:35 +0900

    --amend

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -5,10 +5,12 @@ class Admin::BaseController < ApplicationController
   private
 
   def not_authenticated
+    # reuire_login時のredirect先を以下に変更する
     redirect_to admin_login_path
   end
 
   def admin_user
+    # adminでない場合は、ログインページへリダイレクトさせる
     redirect_to admin_login_path unless current_user.admin?
   end
 end
