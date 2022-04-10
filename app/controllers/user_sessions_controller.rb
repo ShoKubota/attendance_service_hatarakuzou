783f653c4cd67d0667ce136fb5e36b352a5f53fb
ShoKubota <sh2o.1114@mineo.jp>
2022-04-10 13:07:44 +0900

    add: 従業員用ログイン画面を作成 #11

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,20 @@
+class UserSessionsController < ApplicationController
+  skip_before_action :require_login, only: %i[new create]
+
+  def new; end
+
+  def create
+    @user = login(params[:email], params[:password])
+    if @user
+      redirect_back_or_to(admin_users_path)
+    else
+      flash.now[:danger] = 'ログインできませんでした'
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to login_path
+  end
+end
