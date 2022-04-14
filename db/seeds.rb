d5e95a62b0962becbc56709a2a702e80226fb2aa
ShoKubota <sh2o.1114@mineo.jp>
2022-04-14 12:41:57 +0900

    fix: seedファイル更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-30.times do
-  user = User.create!(
+admin_user = User.create!(
+  name: '鈴木一郎'
+  email: 'example@example.com',
+  password: 'password',
+  password_confirmation: 'password',
+  salary: 1200,
+  role: :admin
+)
+
+3.times do
+  general_user = User.create!(
     name: Faker::JapaneseMedia::StudioGhibli.character,
     email: Faker::Internet.email,
     password: 'password',
     password_confirmation: 'password',
     salary: 1200,
-    role: 0
+    role: :general
+  )
+end
+
+1.upto(3) do |i|
+  user = User.find(i)
+  1.upto(10) do |i|
+    approved_attendance = user.attendances.create!(
+      start_time: "2022-04-#{i} 10:00:00",
+      end_time: "2022-04-#{i} 19:00:00",
+      status: :approved
+    )
+  end
+  unapproved_attendance = user.attendances.create!(
+    start_time: "2022-04-14 10:00:00",
+    end_time: "2022-04-14 19:00:00",
+    status: :unapproved
   )
 end
