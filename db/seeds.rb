d1e8f1d266fc4bddafa4c05adc58b618865beef6
ShoKubota <sh2o.1114@mineo.jp>
2022-04-14 12:44:49 +0900

    fix: SyntaxError

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 admin_user = User.create!(
-  name: '鈴木一郎'
+  name: '鈴木一郎',
   email: 'example@example.com',
   password: 'password',
   password_confirmation: 'password',
