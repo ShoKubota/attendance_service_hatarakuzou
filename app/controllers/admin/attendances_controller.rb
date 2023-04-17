57babaa54ed2f6a8fbd5aa9be30ebc0645e9e250
Sho Kubota <osamuraish2o@gmail.com>
2023-04-17 09:26:57 +0900

    add: コメントを追加

--- app/controllers/admin/attendances_controller.rb
+++ app/controllers/admin/attendances_controller.rb
@@ -12,20 +12,21 @@ class Admin::AttendancesController < Admin::BaseController
   end
 
   def approve
-    @attendance.update(status: :approved)
-    flash[:success] = '承認しました！'
-    redirect_to unapproved_admin_attendances_path
+    @attendance.update(status: :approved) # 勤怠データのステータスを承認済みに更新する
+    flash[:success] = '承認しました！' # フラッシュメッセージを表示する
+    redirect_to unapproved_admin_attendances_path # 未承認の勤怠データ一覧にリダイレクトする
   end
 
   def destroy
-    @attendance.destroy!
-    flash[:success] = '勤怠を削除しました！'
-    redirect_to admin_attendances_path
+    @attendance.destroy! # 勤怠データを削除する
+    flash[:success] = '勤怠を削除しました！' # フラッシュメッセージを表示する
+    redirect_to admin_attendances_path # 勤怠データ一覧にリダイレクトする
   end
 
   private
 
   def set_attendance
-    @attendance = Attendance.find(params[:id])
+    @attendance = Attendance.find(params[:id]) # paramsで指定された勤怠データを検索して、@attendanceインスタンス変数に代入する
   end
 end
+
